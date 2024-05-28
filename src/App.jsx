220b0f7d3598933165ce15f8e82bfd0ed0b579e7
Gaurav237 <gktiwary237@gmail.com>
2024-05-29 00:48:03 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemonList, setPokemonList] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/v2/pokemon')
+      .then(response => {
+        setPokemonList(response.data.results);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  const handleSearchChange = event => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredPokemon = pokemonList.filter(pokemon =>
+    pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <h1 className='heading'>Pokemon</h1>
+      <input
+        type="text"
+        placeholder="Search Pokemon"
+        value={searchTerm}
+        onChange={handleSearchChange}
+        className="search-bar"
+      />
+      <div className="pokemon-container">
+        {filteredPokemon.map(pokemon => (
+          <div key={pokemon.url} className="pokemon-card">
+            <img
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${getPokemonId(pokemon.url)}.png`}
+              alt={pokemon.name}
+            />
+            <h3>{pokemon.name}</h3>
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
+}
+
+function getPokemonId(url) {
+  const idRegex = /\/(\d+)\//;
+  const matches = url.match(idRegex);
+  if (matches && matches.length > 1) {
+    return matches[1];
+  }
+  return null;
 }
 
 export default App
